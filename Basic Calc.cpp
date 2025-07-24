058a90fd166491cee82b534dd4c04dc120175d24
OkuJoe <joeokurut776@gmail.com>
2025-07-24 05:58:50 -0700

    Add files via upload

---
+++ Basic Calc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main (){
+
+int x;
+int y;
+cout<< "value of x:" << endl;
+cin>> x;
+cout<<"value of y:"<<endl;
+int sum;
+sum=x+y;
+cout<<"the sum is=" <<sum;
+
+return 0;
+
+
+}
